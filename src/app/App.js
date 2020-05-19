615123d0f2a2988196dac83bdde931c45b9bf338
luciaoSainz <danielsainz28@gmail.com>
2020-05-19 08:51:43 +0200

    added weather api

--- src/app/App.js
+++ src/app/App.js
@@ -2,23 +2,32 @@ import React, { Component } from 'react';
 import WeatherInfo from './components/WeatherInfo';
 import WeatherForm from './components/WeatherForm';
 
+import { WEATHER_KEY } from './keys';
+
+
 class App extends Component {
 
-    getWeather = e => {
-        const {city, country} = e.target.elements;
+    getWeather = async e => {
+        const { city, country } = e.target.elements;
         const cityValue = city.value;
         const countryValue = country.value;
-        console.log(cityValue, countryValue);
+
+
+        const API_URL = `http://api.openweathermap.org/data/2.5/weather?q=${cityValue},${countryValue}& appid=${WEATHER_KEY}`;
+        const response = await fetch(API_URL);
+        const data = response.json();
+        console.log(data);
+
         e.preventDefault();
-    } 
+    }
 
     render() {
         return (
             <div className="container p-4">
                 <div className="row">
                     <div className="col-md-6 mx-auto">
-                       <WeatherForm getWeather={this.getWeather}/>
-                       <WeatherInfo/>
+                        <WeatherForm getWeather={this.getWeather} />
+                        <WeatherInfo />
                     </div>
                 </div>
             </div>
---
+++ src/app/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  WEATHER_KEY:'45531c7f33b81ac3f66e6b2756639247'
+}
