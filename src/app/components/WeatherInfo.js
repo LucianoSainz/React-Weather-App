7dbdb2839569a6ab9b924fba453c450b0d472e71
luciaoSainz <danielsainz28@gmail.com>
2020-05-23 00:07:44 +0200

    added validation

--- src/app/components/WeatherInfo.js
+++ src/app/components/WeatherInfo.js
@@ -11,21 +11,28 @@ const weatherInfo = props => {
                     <p>{props.error}</p>
                 </div>
             }
-
-            < div className="card card-body" >
-                <p>
-                    Location: {props.city}, {props.country}
-                </p>
-                <p>
-                    Temperature: {props.temperature} ºC, {props.description}
-                </p>
-                <p>
-                    Humidity: {props.humidity} %
+            {
+                props.temperature ?
+                    < div className="card card-body" >
+                        <p>
+                            Location: {props.city}, {props.country}
+                        </p>
+                        <p>
+                            Temperature: {props.temperature} ºC, {props.description}
+                        </p>
+                        <p>
+                            Humidity: {props.humidity} %
             </p>
-                <p>
-                    Wind Speed: {props.wind_speed}
-                </p>
-            </div >
+                        <p>
+                            Wind Speed: {props.wind_speed}
+                        </p>
+                    </div >
+                :
+                <div className="card card-body">
+                    <p>No request yet</p>
+
+                </div>
+}
         </div>
     )
 }
