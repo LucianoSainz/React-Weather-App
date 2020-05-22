056342a24e7738ed5e21bd4a9f59ba8df0f9c781
luciaoSainz <danielsainz28@gmail.com>
2020-05-22 23:52:03 +0200

    added error in weather info

--- src/app/components/WeatherInfo.js
+++ src/app/components/WeatherInfo.js
@@ -1,20 +1,31 @@
 import React from 'react';
 
 const weatherInfo = props => {
+
     return (
-        <div className="card card-body">
-            <p>
-                Location: {props.city}, {props.country}
+
+        <div>
+            {
+                props.error &&
+                <div className="alert alert-danger">
+                    <p>{props.error}</p>
+                </div>
+            }
+
+            < div className="card card-body" >
+                <p>
+                    Location: {props.city}, {props.country}
+                </p>
+                <p>
+                    Temperature: {props.temperature} ºC, {props.description}
+                </p>
+                <p>
+                    Humidity: {props.humidity} %
             </p>
-            <p>
-                Temperature: {props.temperature} ºC, {props.description}
-            </p>
-            <p>
-                Humidity: {props.humidity} %
-            </p>
-            <p>
-                Wind Speed: {props.wind_speed}
-            </p>
+                <p>
+                    Wind Speed: {props.wind_speed}
+                </p>
+            </div >
         </div>
     )
 }
