3834fabeec5bb47e2ec66ec93abd3641a5353e7a
luciaoSainz <danielsainz28@gmail.com>
2020-05-28 00:23:52 +0200

    code refactoring

--- src/app/App.js
+++ src/app/App.js
@@ -1,67 +1,21 @@
 import React, { Component } from 'react';
-import WeatherInfo from './components/WeatherInfo';
-import WeatherForm from './components/WeatherForm';
+import Home from './components/Home';
 import Navbar from './components/Navbar';
-
-import { WEATHER_KEY } from './keys';
-
-
-class App extends Component {
-
-    state = {
-        temperature: '',
-        description: '',
-        humidity: '',
-        wind_speed: '',
-        city: '',
-        country: '',
-        error: null
-
-    }
-
-    getWeather = async e => {
-        e.preventDefault();
-        const { city, country } = e.target.elements;
-        const cityValue = city.value;
-        const countryValue = country.value;
+import {Switch, Route} from 'react-router-dom';
 
 
-        if (cityValue && countryValue) {
-            const API_URL = `http://api.openweathermap.org/data/2.5/weather?q=${cityValue},${countryValue}&appid=${WEATHER_KEY}&units=metric`;
-            const response = await fetch(API_URL);
-            const data = await response.json();
-            console.log(data);
 
-            this.setState({
-                temperature: data.main.temp,
-                description: data.weather[0].description,
-                humidity: data.main.humidity,
-                wind_speed: data.wind.speed,
-                city: data.name,
-                country: data.sys.country,
-                error: null
-            });
-        } else {
-            this.setState({ error: 'Please Enter a City and Country' })
-
-        }
-    }
-
-    render() {
-        return (
-            <>
-            <Navbar />
-            <div className="container p-4">
-                <div className="row">
-                    <div className="col-md-6 mx-auto">
-                        <WeatherForm getWeather={this.getWeather} />
-                        <WeatherInfo {...this.state} />
-                    </div>
-                </div>
-            </div>
-            </>
-        )
-    }
-}
-
-export default App;
+const App = () => {
+    return (
+      <>
+        <Navbar />
+  
+        <Switch>
+           <Route path="/" exact component={Home} />
+           
+        </Switch>
+      </>
+    )
+  }
+  
+  export default App;
