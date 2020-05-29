ca342ee7443e9f274eb7dfebd2cc39caa5e5da79
luciaoSainz <danielsainz28@gmail.com>
2020-05-29 17:25:36 +0200

    added weather in App

--- src/app/App.js
+++ src/app/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import Home from './components/Home';
+import Weather from './components/Weather';
 import AboutUs from './components/AboutUs';
 import Navbar from './components/Navbar';
+import Carousel from './components/carousel';
 import {Switch, Route} from 'react-router-dom';
 
 
@@ -10,12 +12,14 @@ const App = () => {
     return (
       <>
         <Navbar />
-  
+
         <Switch>
            <Route path="/" exact component={Home} />
+           <Route path="/Weather" exact component={Weather} />
            <Route path="/AboutUs" exact component={AboutUs} />
-        </Switch>
+        </Switch>  
       </>
+      
     )
   }
   
