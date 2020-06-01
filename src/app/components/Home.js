dad6430dd7bc1d16576f4a7b3b3d5b3cefdfce8e
luciaoSainz <danielsainz28@gmail.com>
2020-06-01 12:00:12 +0200

    I fixed the carrousel and it works

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -1,37 +1,40 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Carousel } from 'react-bootstrap';
 
-const Carousel = () => {
+const Home = () => {
+  
 
   return (
 
-<div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
-  <ol className="carousel-indicators">
-    <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-  </ol>
-  <div className="carousel-inner">
-    <div className="carousel-item active">
-      <img src="src/app/components/images/sol.jpg" className="d-block w-100"  height="400" alt="..." />
-    </div>
-    <div className="carousel-item">
-      <img src="src/app/components/images/sol.jpg" className="d-block w-100" alt="..." />
-    </div>
-    <div className="carousel-item">
-      <img src="src/app/components/images/rayos.jpg" className="d-block w-100" alt="..." />
-    </div>
-  </div>
-  <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span className="sr-only">Previous</span>
-  </a>
-  <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-    <span className="sr-only">Next</span>
-  </a>
-</div>
+    <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"  height="400px"
+          src="src/app/components/images/slide1.jpg"
+          alt="First slide"
+        />
+
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100" height="400px"
+          src="src/app/components/images/slide2.jpg"
+          alt="Third slide"
+        />
+
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"  height="400px"
+          src="src/app/components/images/slide3.jpg"
+          alt="Third slide"
+        />
+
+      </Carousel.Item>
+    </Carousel>
+    
+  
   )
 }
 
-export default Carousel;
+export default Home;
