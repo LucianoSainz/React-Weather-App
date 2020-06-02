f9bfe1c03b77e66601fea984c4b3b1e0c5cd0f44
luciaoSainz <danielsainz28@gmail.com>
2020-06-03 00:38:37 +0200

    added how our app works in home

--- src/app/components/Component.js
+++ src/app/components/Component.js
@@ -7,6 +7,12 @@ const Component = () => {
     return (
         <Container>
             <h2>Welcome to weather app</h2>
+            <h4>how our app works</h4>
+            <ol>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ol>
         </Container>
     )
 }
